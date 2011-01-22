56ff7646cae6140a9bf84c37ad1677eab12da2ee
demon <demon@2888f6ba-35ad-4816-ac48-6755f7664153>
2011-01-22 00:44:14 +0000

    Second round of FlaggedRevs merging: r78520 r79872 r79873 r80297

--- extensions/FlaggedRevs/FlaggedRevs.php
+++ extensions/FlaggedRevs/FlaggedRevs.php
@@ -250,7 +250,7 @@ $wgAvailableRights[] = 'movestable';
 $wgAvailableRights[] = 'stablesettings';
 
 # Bump this number every time you change flaggedrevs.css/flaggedrevs.js
-$wgFlaggedRevStyleVersion = 84;
+$wgFlaggedRevStyleVersion = 85;
 
 $wgExtensionFunctions[] = 'efLoadFlaggedRevs';
 
