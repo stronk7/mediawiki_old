70236ca72ce21126190ef97b6d2145c8f6a91b99
tstarling <tstarling@2888f6ba-35ad-4816-ac48-6755f7664153>
2011-05-05 03:15:45 +0000

    Fixed some register_globals vulnerabilities.

--- extensions/CreateRedirect/CreateRedirect.body.php
+++ extensions/CreateRedirect/CreateRedirect.body.php
@@ -28,7 +28,7 @@
 if ( !defined( 'MEDIAWIKI' ) ) {
 	echo <<<EOT
 To install the CreateRedirect extension, put the following line in LocalSettings.php:
-require_once( "$IP/extensions/CreateRedirect/CreateRedirect.php" );
+require_once( "\$IP/extensions/CreateRedirect/CreateRedirect.php" );
 EOT;
 	exit( 1 );
 }
