5cc585f9ecc47fa1a11b0bae32a0d8ebe29bd13b
faurethomas <faurethomas@2888f6ba-35ad-4816-ac48-6755f7664153>
2011-05-12 13:15:32 +0000

    backport Categorize to v0.1.2

--- extensions/Categorize/Categorize.i18n.php
+++ extensions/Categorize/Categorize.i18n.php
@@ -4,26 +4,27 @@
 /* Categorize Mediawiki Extension
  *
  * @author Andreas Rindler (mediawiki at jenandi dot com) and Thomas Fauré (faure dot thomas at gmail dot com)
- * @licence GNU General Public Licence 2.0 or later
+ * @licence GNU General Public Licence 3.0
  * @description 
  *
 */
 $messages = array();
 
+/** English
+ * @author Thomas Fauré
+ */
 $messages['en'] = array(
 	'categorize-title'       => 'Knowledge qualification',
 	'categorize-subtitle'    => '',
 	'categorize-footer'      => 'If you want to add a label, please contact us at ...',
-	'categorize-boxlabel'    => 'Cat&eacute;gories',
-	'categorize-taglabel'    => 'Cat&eacute;gories populaires',
 	'categorize-advice'      => 'Click on the labels you want to attribute to this article:'
 	);
+/** French (Français)
+ * @author Thomas Fauré
+ */
 $messages['fr'] = array(
 	'categorize-title'       => 'Qualification des savoirs',
 	'categorize-subtitle'    => '',
 	'categorize-footer'      => 'Si vous souhaitez ajouter un label, contactez ...',
-	'categorize-boxlabel'    => 'Cat&eacute;gories',
-	'categorize-taglabel'    => 'Cat&eacute;gories populaires',
-	'categorize-advice'      => 'Cliquez sur les labels que vous souhaitez attribuer &agrave; cet article :'
+	'categorize-advice'      => 'Cliquez sur les labels que vous souhaitez attribuer à cet article :'
 	);
-?>
