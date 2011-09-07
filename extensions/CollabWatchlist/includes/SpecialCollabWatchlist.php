0def59036c36a94827ddc78368d228027e89fffa
catrope <catrope@2888f6ba-35ad-4816-ac48-6755f7664153>
2011-09-07 21:56:51 +0000

    1.18: MFT r95562, r95570, r95597, r95608, r95647, r95648, r95674, r95790, r95999, r96087

--- extensions/CollabWatchlist/includes/SpecialCollabWatchlist.php
+++ extensions/CollabWatchlist/includes/SpecialCollabWatchlist.php
@@ -197,7 +197,8 @@ class SpecialCollabWatchlist extends SpecialPage {
 		}
 	
 		# Show a message about slave lag, if applicable
-		if( ( $lag = $dbr->getLag() ) > 0 )
+		$lag = wfGetLB()->safeGetLag( $dbr );
+		if ( $lag > 0 )
 			$wgOut->showLagWarning( $lag );
 	
 		# Create output form
