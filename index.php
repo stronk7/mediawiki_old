8f3f5fde26e80b1ac9d8cf0d87225e2343867ce1
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-10-06 01:50:15 +0200

    Add current top-level files

---
+++ index.php
@@ -0,0 +1,13 @@
+<head>
+<meta http-equiv="refresh" content="0;URL=http://docs.moodle.org/en" />
+</head>
+
+<body>
+<center>
+
+<h1>Moodle Documentation</h1>
+
+<h2><a href="/en/">English</a></h2>
+
+</center>
+</body>
---
+++ overview/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <meta name="keywords" content="Main Page,About Moodle,Administrator documentation,Bulk user actions,Developer documentation,GSOC/2008,Grades,Notes,Sandbox,Teacher documentation,Upgrading to Moodle 1.9" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="search" type="application/opensearchdescription+xml" href="/en/opensearch_desc.php" title="MoodleDocs" />
+        <link title="Creative Commons" type="application/rdf+xml" href="/en/index.php?title=Main_Page&amp;action=creativecommons" rel="meta" />
+        <link rel="copyright" href="http://docs.moodle.org/en/License" />
+        <title>Moodle Docs overview</title>
+
+        <style type="text/css" media="screen, projection">/*<![CDATA[*/
+            @import "/20/en/skins/common/shared.css?97";
+            @import "/20/en/skins/moodledocs/main.css?97";
+            @import "/20/en/skins/moodledocs/moodledocs.css?97";
+        /*]]>*/</style>
+        <link rel="stylesheet" type="text/css" media="print" href="/20/en/skins/common/commonPrint.css?97" />
+        <!--[if lt IE 5.5000]><style type="text/css">@import "/20/en/skins/moodledocs/IE50Fixes.css?97";</style><![endif]-->
+        <!--[if IE 5.5000]><style type="text/css">@import "/20/en/skins/moodledocs/IE55Fixes.css?97";</style><![endif]-->
+        <!--[if IE 6]><style type="text/css">@import "/20/en/skins/moodledocs/IE60Fixes.css?97";</style><![endif]-->
+        <!--[if IE 7]><style type="text/css">@import "/20/en/skins/moodledocs/IE70Fixes.css?97";</style><![endif]-->
+        <!--[if lt IE 7]><script type="text/javascript" src="/20/en/skins/common/IEFixes.js?97"></script>
+            <meta http-equiv="imagetoolbar" content="no" /><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="/20/en/skins/moodledocs/moodle/moodle.css" />
+
+<link rel="stylesheet" type="text/css" href="/20/en/skins/moodledocs/moodle/menu.css" />
+<!--[if IE 6]><style type="text/css">@import "/20/en/skins/moodledocs/moodle/IE60Fixes.css";</style><![endif]-->
+<!--[if IE 5.5000]><style type="text/css">@import "/20/en/skins/moodledocs/moodle/IE55Fixes.css";</style><![endif]-->
+
+<script type="text/javascript" src="/20/en/skins/moodledocs/moodle/sm/c_config.js"></script>
+<script type="text/javascript" src="/20/en/skins/moodledocs/moodle/sm/c_smartmenus.js"></script>
+
+        <script type= "text/javascript">/*<![CDATA[*/
+var skin = "moodledocs";
+var stylepath = "/20/en/skins";
+var wgArticlePath = "/en/$1";
+var wgScriptPath = "/en";
+var wgScript = "/en/index.php";
+var wgServer = "http://docs.moodle.org";
+var wgCanonicalNamespace = "";
+var wgCanonicalSpecialPageName = false;
+var wgNamespaceNumber = 0;
+var wgPageName = "Main_Page";
+var wgTitle = "Main Page";
+var wgAction = "view";
+var wgRestrictionEdit = ["sysop"];
+var wgRestrictionMove = ["sysop"];
+var wgArticleId = "1";
+var wgIsArticle = true;
+var wgUserName = "Dougiamas";
+var wgUserGroups = ["bureaucrat", "sysop", "*", "user", "autoconfirmed", "emailconfirmed"];
+var wgUserLanguage = "en";
+var wgContentLanguage = "en";
+var wgBreakFrames = false;
+var wgCurRevisionId = "41590";
+var wgAjaxWatch = {"watchMsg": "Watch", "unwatchMsg": "Unwatch", "watchingMsg": "Watching...", "unwatchingMsg": "Unwatching..."};
+/*]]>*/</script>
+                
+        <script type="text/javascript" src="/20/en/skins/common/wikibits.js?97"><!-- wikibits js --></script>
+        <script type="text/javascript" src="/en/index.php?title=-&amp;action=raw&amp;smaxage=0&amp;gen=js&amp;useskin=moodledocs"><!-- site js --></script>
+
+        <style type="text/css">/*<![CDATA[*/
+@import "/en/index.php?title=MediaWiki:Common.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000";
+@import "/en/index.php?title=MediaWiki:Moodledocs.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000";
+@import "/en/index.php?title=-&action=raw&gen=css&maxage=18000&smaxage=0";
+/*]]>*/</style>
+        <!-- Head Scripts -->
+        <script type="text/javascript" src="/20/en/skins/common/ajax.js?97"></script>
+        <script type="text/javascript" src="/20/en/skins/common/ajaxwatch.js?97"></script>
+    <style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "/overview/extra.css"; /*]]>*/</style>
+    <style type="text/css">
+ul.lang-list {
+  width: 75%;
+  margin: 0 auto; 
+  padding: 10px;
+
+}
+ul.lang-list li {
+  width: 50%;
+  margin: 0;
+  padding: 5px 0;
+  text-align: center;
+  float: left;
+  font-size: 1.2em;
+  list-style: none;
+}
+p.lang-clearer {
+  padding: 20px 20px 10px 20px;
+  margin: 0;
+  clear: both;
+  text-align:center;
+}
+h2 {
+  text-align:center;
+}
+    </style>
+    </head>
+<body  class="mediawiki ns-0 ltr page-Main_Page">
+
+<div id="page">
+  <div id="moodlelogo">
+
+    <a href="http://moodle.org/">
+      <img class="logo" src="/20/en/skins/moodledocs/moodle/pix/moodle-logo.gif" border="0" alt="moodlelogo" title="moodle.org" />
+    </a>
+  </div>
+
+<div id="moodlemenu" class="clearfix">
+<?php include('/var/www/vhosts/docs.moodle.org/html/prodwiki/skins/moodledocs/moodle/menu.html');  ?>
+    <form id="global-search" method="get" action="http://moodle.org/public/search">
+        <div>
+            <input type="hidden" name="cx" value="017878793330196534763:-0qxztjngoy" />
+            <input type="hidden" name="cof" value="FORID:9" />
+            <input type="hidden" name="ie" value="UTF-8" />
+            <input class="input-text" type="text" name="q" size="15" maxlength="255"/>
+            <input class="input-submit" type="submit" name="sa" value="Search moodle.org"/>
+        </div>
+    </form>
+</div>
+
+<div class="navbar clearfix">
+ <div class="breadcrumb"><h2 class="accesshide">You are here</h2>
+
+  <ul>
+   <li class="first"><a href="http://moodle.org">Home</a></li>
+   <li class="first"><span class="accesshide " >/&nbsp;</span><span class="arrow sep">&#x25BA;</span> Moodle Docs</li>
+  </ul>
+
+ </div>
+</div>
+
+
+<div id="globalWrapper">
+
+
+      <?php include('content.html');  ?>
+
+  <div class="visualClear"></div>
+  <div id="footer">
+     <ul id="f-list">
+       <li id="copyright">Content is available under <a href="http://docs.moodle.org/en/License" class="external " title="http://docs.moodle.org/en/License" rel="nofollow">GNU Public License</a>.</li>
+       <li id="about"><a href="/en/MoodleDocs:About" title="MoodleDocs:About">About MoodleDocs</a></li>
+       <li id="disclaimer"><a href="/en/MoodleDocs:General_disclaimer" title="MoodleDocs:General disclaimer">Disclaimers</a></li>
+     </ul>
+  </div>
+
+        
+<!-- Served in 0.062 secs. -->  <div id="moodlesitelink">
+    <a href="http://moodle.org/">
+      <img width="100" height="30" src="/20/en/skins/moodledocs/moodle/pix/moodle-logo-footer.gif" border="0" alt="moodlelogo" title="Return to Moodle.org" />
+    </a>
+
+</div>
+</div>
+</div>
+</body></html>
