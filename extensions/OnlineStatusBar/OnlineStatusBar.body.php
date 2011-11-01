862d0fc16fa4ed7949a85588da1adb1791a97420
petrb <petrb@2888f6ba-35ad-4816-ac48-6755f7664153>
2011-11-01 08:48:23 +0000

    created download for extension in 1.18 branch

---
+++ extensions/OnlineStatusBar/OnlineStatusBar.body.php
@@ -0,0 +1,212 @@
+<?
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a part of mediawiki and can't be started separately";
+	die();
+}
+
+/**
+ * Main file of Online status bar extension.
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Petr Bena <benapetr@gmail.com>
+ * @author of magic word Alexandre Emsenhuber
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link http://www.mediawiki.org/wiki/Extension:OnlineStatusBar Documentation
+ */
+
+class OnlineStatusBar {
+
+	public static function getStatusBarHtml( $text ) {
+		return <<<HTML
+<div class="onlinestatusbarbody metadata onlinestatusbartop" id="status-top">
+<div class="onlinestatusbaricon">
+$text</div></div>
+HTML;
+	}
+
+	/**
+	 * Returns image element
+	 *
+	 * @param $mode String
+	 * @return string
+	 */
+	public static function GetImageHtml( $mode ) {
+		global $wgExtensionAssetsPath, $wgOnlineStatusBarIcon, $wgOnlineStatusBarModes;
+		$icon = "$wgExtensionAssetsPath/OnlineStatusBar/{$wgOnlineStatusBarIcon[$mode]}";
+		return Html::element( 'img', array( 'src' => $icon ) );
+	}
+
+
+	/**
+	 * Returns the status and User element
+	 *
+	 * @param Title $title
+	 * @return array|bool Array containing the status and User object
+         */
+	public static function getAnonFromTitle( Title $title ) {
+		global $wgOnlineStatusBarTrackIpUsers;
+		if ( $wgOnlineStatusBarTrackIpUsers == false ) {
+			return false;
+		}
+	
+		if ( $title->getNamespace() != NS_USER && $title->getNamespace() != NS_USER_TALK ) {
+			return false;
+		}
+
+		$user = User::newFromId( 0 );
+		$user->setName( $title->getBaseText() );
+
+		// Check if something wrong didn't happen
+		if ( $user === false ) {
+			return false;
+		}
+
+		$status = self::getStatus( $user );
+
+		return array( $status, $user );
+	}
+
+
+	/**
+	 * Returns the status and User element
+	 *
+	 * @param Title $title
+	 * @return array|bool Array containing the status and User object
+	 */
+	public static function getUserInfoFromTitle( Title $title ) {
+		if ( $title->getNamespace() != NS_USER && $title->getNamespace() != NS_USER_TALK ) {
+			return false;
+		}
+
+		$user = User::newFromName( $title->getBaseText() );
+		// Invalid user
+		if ( $user === false ) {
+			return false;
+		}
+		if ( !self::isValid( $user ) ) {
+			return false;
+		}
+
+		$status = self::getStatus( $user );
+
+		return array( $status, $user );
+	}
+
+	/**
+	 * @param $user User
+	 * @return String
+	 */
+	public static function getStatus( $user, $update = false ) {
+		global $wgOnlineStatusBarDefaultOffline, $wgOnlineStatusBarDefaultOnline;
+		// remove old entries
+		if ( $update )
+		{
+			self::DeleteOld();
+		}
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$result = $dbr->selectField( 'online_status', 'username', array( 'username' => $user->getName() ),
+			__METHOD__, array( 'LIMIT 1', 'ORDER BY timestamp DESC' ) );
+
+		if ( $result === false ) {
+			$status = $wgOnlineStatusBarDefaultOffline;
+		} else {
+			// let's check if it isn't anon
+			if ( $user->isLoggedIn() ) {
+				$status = $user->getOption( 'OnlineStatusBar_status', $wgOnlineStatusBarDefaultOnline );
+			} else {
+				$status = $wgOnlineStatusBarDefaultOnline;
+			}
+		}
+
+		if ( $status == 'hidden' ) {
+			$status = 'offline';
+		}
+
+		return $status;
+	}
+
+	/**
+	 * Insert to the database
+	 * @return bool
+	 */
+	public static function UpdateDb() {
+		global $wgUser, $wgOnlineStatusBarDefaultOnline;
+		// Skip users we don't track
+		if ( self::IsValid ( $wgUser ) != true ) {
+			return false;
+		}
+		// If we track them, let's insert it to the table 
+		$dbw = wfGetDB( DB_MASTER );
+		$row = array(
+			'username' => $wgUser->getName(),
+			'timestamp' => $dbw->timestamp(),
+		);
+		$dbw->insert( 'online_status', $row, __METHOD__, 'DELAYED' );
+		return false;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function UpdateStatus() {
+		global $wgUser, $wgOnlineStatusBarDefaultOffline;
+		if ( OnlineStatusBar::GetStatus( $wgUser, true ) == $wgOnlineStatusBarDefaultOffline ) {
+			OnlineStatusBar::UpdateDb();
+			return true;
+		}
+
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->update(
+			'online_status',
+			array( 'timestamp' => $dbw->timestamp() ),
+			array( 'username' => $wgUser->getName() ),
+			__METHOD__
+		);
+
+		return false;
+	}
+
+	/**
+	 * @return int
+	 */
+	public static function DeleteOld() {
+		global $wgOnlineStatusBar_LogoutTime;
+		$dbw = wfGetDB( DB_MASTER );
+		// calculate time and convert it back to mediawiki format
+		$time = wfTimestamp( TS_UNIX ) - $wgOnlineStatusBar_LogoutTime;
+		$dbw->delete( 'online_status', array( "timestamp < " . $dbw->addQuotes( $dbw->timestamp( $time ) ) ), __METHOD__ );
+		return 0;
+	}
+
+
+	/**
+	 * Checks to see if the user can be tracked
+	 *
+	 * @param User $user
+	 * @return bool
+	 */
+	public static function isValid( User $user ) {
+		global $wgOnlineStatusBarTrackIpUsers, $wgOnlineStatusBarDefaultEnabled;
+
+		// checks if anon
+		if ( $user->isAnon() ) {
+			return $wgOnlineStatusBarTrackIpUsers;
+		}
+
+		// do we track them
+		return $user->getOption( "OnlineStatusBar_active", $wgOnlineStatusBarDefaultEnabled );
+	}
+
+	/**
+	 * Delete user who logged out
+	 * @param $userName string
+	 * @return bool
+	 */
+	static function DeleteStatus( $userName ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->delete( 'online_status', array( 'username' => $userName ), __METHOD__ ); // delete user
+		return true;
+	}
+}
---
+++ extensions/OnlineStatusBar/OnlineStatusBarHooks.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * Hooks for OnlineStatusBar
+ *
+ * @group Extensions
+ */
+class OnlineStatusBarHooks {
+	/**
+	 * @param DatabaseUpdater|null $updater
+	 * @return bool
+	 */
+	public static function ckSchema( $updater = null ) {
+		if ( $updater !== null ) {
+			$updater->addExtensionUpdate( array( 'addtable', 'online_status', dirname( __FILE__ ) . '/OnlineStatusBar.sql', true ) );
+		} else {
+			global $wgExtNewTables;
+			$wgExtNewTables[] = array(
+				'online_status', dirname( __FILE__ ) . '/OnlineStatusBar.sql' );
+		}
+		return true;
+	}
+
+	/**
+	 * @param $user User
+	 * @param $inject_html string
+	 * @param $old_name string
+	 * @return bool
+	 */
+	public static function logout( &$user, &$inject_html, $old_name ) {
+		OnlineStatusBar::DeleteStatus( $old_name );
+		return true;
+	}
+
+	/**
+	 * Called everytime when it's needed to update db
+	 * @return bool
+	 */
+	public static function updateStatus() {
+		OnlineStatusBar::UpdateDb();
+		return true;
+	}
+
+	/**
+	 * Creates a bar
+	 * @param $article Article
+	 * @param $outputDone bool
+	 * @param $pcache string
+	 * @return bool
+	 */
+	public static function renderBar( &$article, &$outputDone, &$pcache ) {
+		$context = $article->getContext();
+
+		OnlineStatusBar::UpdateStatus();
+		$result = OnlineStatusBar::getUserInfoFromTitle( $article->getTitle() );
+		if ( $result === false && User::isIP ( $article->getTitle()->getBaseText() ) ) {
+			$result = OnlineStatusBar::getAnonFromTitle( $article->getTitle() ); 
+		}
+
+		if ( $result === false ) {
+			return true;
+		}
+
+		/** @var $user User */
+		list( $status, $user ) = $result;
+
+		// Don't display status of those who have opted out
+		if ( $user->getOption( 'OnlineStatusBar_hide' ) == true ) {
+			return true;
+		}
+
+		$modetext = wfMessage( 'onlinestatusbar-status-' . $status )->toString();
+		$image = OnlineStatusBar::getImageHtml( $status );
+		$text = wfMessage( 'onlinestatusbar-line', $user->getName() )
+				->rawParams( $image )->params( $modetext )->escaped();
+		$context->getOutput()->addHtml( OnlineStatusBar::getStatusBarHtml( $text ) );
+
+		return true;
+	}
+
+	/**
+	 * Insert user options
+	 * @param $user User
+	 * @param $preferences array
+	 * @return bool
+	 */
+	public static function preferencesHook( User $user, array &$preferences ) {
+		global $wgOnlineStatusBarDefaultOnline, $wgOnlineStatusBarDefaultEnabled, $wgOnlineStatusBarModes;
+		$preferences['OnlineStatusBar_active'] = array( 'type' => 'toggle', 'label-message' => 'onlinestatusbar-used', 'section' => 'misc/onlinestatus' );
+		$preferences['OnlineStatusBar_hide'] = array( 'type' => 'toggle', 'label-message' => 'onlinestatusbar-hide', 'section' => 'misc/onlinestatus' );
+		$preferences['OnlineStatusBar_status'] = array( 'type' => 'radio', 'label-message' => 'onlinestatusbar-status', 'section' => 'misc/onlinestatus',
+			'options' => array(
+				wfMessage( 'onlinestatusbar-status-online' )->escaped() => 'online',
+				wfMessage( 'onlinestatusbar-status-busy' )->escaped() => 'busy',
+				wfMessage( 'onlinestatusbar-status-away' )->escaped() => 'away',
+				wfMessage( 'onlinestatusbar-status-hidden' )->escaped() => 'hidden'
+			),
+		);
+		return true;
+	}
+
+	/**
+	 * @param $defaultOptions array
+	 * @return bool
+	 */
+	public static function setDefaultOptions( &$defaultOptions ) {
+		global $wgOnlineStatusBarDefaultOnline, $wgOnlineStatusBarDefaultEnabled;
+		// set defaults
+		$defaultOptions['OnlineStatusBar_status'] = $wgOnlineStatusBarDefaultOnline;
+		$defaultOptions['OnlineStatusBar_active'] = $wgOnlineStatusBarDefaultEnabled;
+		$defaultOptions['OnlineStatusBar_hide'] = false;
+		// quit
+		return true;
+	}
+
+	/**
+	 * @param $magicWords array
+	 * @param $ln string (language)
+	 * @return bool
+	 */
+	public static function magicWordVar( array &$magicWords, $ln ) {
+		$magicWords['isonline'] = array( 0, 'isonline' );
+		return true;
+	}
+
+	/**
+	 * @param $out OutputPage
+	 * @param $skin Skin
+	 * @return bool
+	 */
+	public static function stylePage( &$out, &$skin ) {
+		$out->addModules( 'ext.OnlineStatusBar' );
+		return true;
+	}
+
+	/**
+	 * @param $vars array
+	 * @return bool
+	 */
+	public static function magicWordSet( &$vars ) {
+		$vars[] = 'isonline';
+		return true;
+	}
+
+	/**
+	 * @param $parser Parser
+	 * @param $varCache ??
+	 * @param $index ??
+	 * @param $ret string?
+	 * @return bool
+	 */
+	public static function parserGetVariable( &$parser, &$varCache, &$index, &$ret ) {
+		if ( $index != 'isonline' ) {
+			return true;
+		}
+
+		$result = OnlineStatusBar::getUserInfoFromTitle( $parser->getTitle() );
+		if ( User::isIP( $parser->getTitle()->getBaseText() ) && $result == null ) {
+			$result = OnlineStatusBar::getAnonFromTitle( $parser->getTitle() );
+		}
+		
+		if ( $result == false ) {
+			$ret = "unknown";
+			return true;
+		}
+
+		$ret = $result[0];
+		return true;
+	}
+}
