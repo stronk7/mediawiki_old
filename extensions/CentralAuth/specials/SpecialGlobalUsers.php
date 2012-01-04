9c09f123efcf891f7312485214574cf2e9a24346
reedy <reedy@2888f6ba-35ad-4816-ac48-6755f7664153>
2012-01-04 19:16:56 +0000

    Fix r104178 like r108070

--- extensions/CentralAuth/specials/SpecialGlobalUsers.php
+++ extensions/CentralAuth/specials/SpecialGlobalUsers.php
@@ -9,7 +9,7 @@ class SpecialGlobalUsers extends SpecialPage {
 		global $wgOut, $wgRequest, $wgContLang;
 		$this->setHeaders();
 
-		$pg = new GlobalUsersPager( $this->getContext(), $par );
+		$pg = new GlobalUsersPager( $par );
 
 		if ( $par ) {
 			$pg->setGroup( $par );
@@ -33,8 +33,8 @@ class SpecialGlobalUsers extends SpecialPage {
 class GlobalUsersPager extends UsersPager {
 	protected $requestedGroup = false, $requestedUser;
 
-	function __construct( IContextSource $context = null, $par = null ) {
-		parent::__construct( $context, $par );
+	function __construct( $par = null ) {
+		parent::__construct( $par );
 		$this->mDb = CentralAuthUser::getCentralSlaveDB();
 	}
 
