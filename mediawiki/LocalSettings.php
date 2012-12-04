2b4abf201269d850955b6f52799d1abed9267e3d
Dan Poltawski <dan@moodle.com>
2012-12-04 16:12:34 +0800

    MDLSITE-1971 - reinstate email ntofiications on 2.4 wiki

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -87,7 +87,8 @@ if (php_sapi_name() != 'cli') {
             $mdocsver = '23';
         }else if (substr($_SERVER['REQUEST_URI'], 1, 2) === '24') {
             $mdocsver = '24';
-            $wgEnableEmail = false;
+            # Disable this for email notifications:
+            #            $wgEnableEmail = false;
         }else if (substr($_SERVER['REQUEST_URI'], 1, 2) === '2x') {
             $mdocsver = '2x';
         }else if (substr($_SERVER['REQUEST_URI'], 1, 3) === 'all') {
