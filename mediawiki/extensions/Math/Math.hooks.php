19b316d4e19c51447dcd77c511365ff2fd252091
David Mudrák <david@moodle.com>
2013-07-26 09:25:07 +0200

    Include the snapshot of the Math extension master branch

---
+++ mediawiki/extensions/Math/Math.hooks.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * MediaWiki math extension
+ *
+ * (c) 2002-2012 various MediaWiki contributors
+ * GPLv2 license; info in main package.
+ */
+
+class MathHooks {
+	/**
+	 * Set up $wgMathPath and $wgMathDirectory globals if they're not already
+	 * set.
+	 */
+	static function setup() {
+		global $wgMathPath, $wgMathDirectory;
+		global $wgUploadPath, $wgUploadDirectory;
+		if ( $wgMathPath === false ) {
+			$wgMathPath = "{$wgUploadPath}/math";
+		}
+		if ( $wgMathDirectory === false ) {
+			$wgMathDirectory = "{$wgUploadDirectory}/math";
+		}
+	}
+
+	/**
+	 * Register the <math> tag with the Parser.
+	 *
+	 * @param $parser Parser instance of Parser
+	 * @return Boolean: true
+	 */
+	static function onParserFirstCallInit( $parser ) {
+		$parser->setHook( 'math', array( 'MathHooks', 'mathTagHook' ) );
+		return true;
+	}
+
+	/**
+	 * Callback function for the <math> parser hook.
+	 *
+	 * @param $content (the LaTeX input)
+	 * @param $attributes
+	 * @param $parser Parser
+	 * @return string
+	 */
+	static function mathTagHook( $content, $attributes, $parser ) {
+		global $wgContLang, $wgUseMathJax;
+		if ( trim( $content )  === "" ) { // bug 8372
+			return "";
+		}
+		$mode = $parser->getOptions()->getMath();
+		$renderer = MathRenderer::getRenderer(
+			$content, $attributes, $mode
+		);
+		$renderedMath = $renderer->render();
+		if ( $wgUseMathJax && $mode == MW_MATH_MATHJAX ) {
+			$parser->getOutput()->addModules( array( 'ext.math.mathjax.enabler' ) );
+		}
+		$renderer->writeCache();
+		return $wgContLang->armourMath( $renderedMath );
+	}
+
+	/**
+	 * Add the new math rendering options to Special:Preferences.
+	 *
+	 * @param $user Object: current User object
+	 * @param $defaultPreferences Object: Preferences object
+	 * @return Boolean: true
+	 */
+	static function onGetPreferences( $user, &$defaultPreferences ) {
+		$defaultPreferences['math'] = array(
+			'type' => 'radio',
+			'options' => array_flip( self::getMathNames() ),
+			'label' => '&#160;',
+			'section' => 'rendering/math',
+		);
+		return true;
+	}
+
+	/**
+	 * List of message keys for the various math output settings.
+	 *
+	 * @return array of strings
+	 */
+	private static function getMathNames() {
+		global $wgUseMathJax, $wgUseLaTeXML;
+		$names = array(
+			MW_MATH_PNG => wfMessage( 'mw_math_png' )->escaped(),
+			MW_MATH_SOURCE => wfMessage( 'mw_math_source' )->escaped(),
+		);
+		if ( $wgUseMathJax ) {
+			$names[MW_MATH_MATHJAX] = wfMessage( 'mw_math_mathjax' )->escaped();
+		}
+		if ( $wgUseLaTeXML ) {
+			$names[MW_MATH_LATEXML] = wfMessage( 'mw_math_latexml' )->escaped();
+		}
+		return $names;
+	}
+
+	/**
+	 * MaintenanceRefreshLinksInit handler; optimize settings for refreshLinks batch job.
+	 *
+	 * @param Maintenance $maint
+	 * @return boolean hook return code
+	 */
+	static function onMaintenanceRefreshLinksInit( $maint ) {
+		global $wgUser;
+
+		# Don't generate TeX PNGs (lack of a sensible current directory causes errors anyway)
+		$wgUser->setOption( 'math', MW_MATH_SOURCE );
+		return true;
+	}
+
+	/**
+	 * LoadExtensionSchemaUpdates handler; set up math table on install/upgrade.
+	 *
+	 * @param $updater DatabaseUpdater
+	 * @throws MWException
+	 * @return bool
+	 */
+	static function onLoadExtensionSchemaUpdates( $updater = null ) {
+		if ( is_null( $updater ) ) {
+			throw new MWException( "Math extension is only necessary in 1.18 or above" );
+		}
+		$map = array(
+			'mysql' => 'math.sql',
+			'sqlite' => 'math.sql',
+			'postgres' => 'math.pg.sql',
+			'oracle' => 'math.oracle.sql',
+			'mssql' => 'math.mssql.sql',
+			'db2' => 'math.db2.sql',
+		);
+		$type = $updater->getDB()->getType();
+		if ( isset( $map[$type] ) ) {
+			$sql = dirname( __FILE__ ) . '/db/' . $map[$type];
+			$updater->addExtensionTable( 'math', $sql );
+		} else {
+			throw new MWException( "Math extension does not currently support $type database." );
+		}
+		return true;
+	}
+
+	/**
+	 * Add 'math' table to the list of tables that need to be copied to
+	 * temporary tables for parser tests to run.
+	 *
+	 * @param array $tables
+	 * @return bool
+	 */
+	static function onParserTestTables( &$tables ) {
+		$tables[] = 'math';
+		return true;
+	}
+
+	/**
+	 * Hack to fake a default $wgMathPath value so parser test output
+	 * that renders to images doesn't vary by who runs it.
+	 *
+	 * @global string $wgMathPath
+	 * @param Parser $parser
+	 * @return bool
+	 */
+	static function onParserTestParser( &$parser ) {
+		global $wgMathPath;
+		$wgMathPath = '/images/math';
+		return true;
+	}
+
+	/**
+	 * Links to the unit test files for the test cases.
+	 *
+	 * @param string $files
+	 * @return boolean (true)
+	 */
+	static function onRegisterUnitTests( &$files ) {
+		$testDir = __DIR__ . '/tests/';
+		$files = array_merge( $files, glob( "$testDir/*Test.php" ) );
+		return true;
+	}
+}
---
+++ mediawiki/extensions/Math/Math.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * MediaWiki math extension
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Tomasz Wegrzanowski
+ * @author Brion Vibber
+ * @author Moritz Schubotz
+ * @copyright © 2002-2012 various MediaWiki contributors
+ * @license GPLv2 license; info in main package.
+ * @link http://www.mediawiki.org/wiki/Extension:Math Documentation
+ * @see https://bugzilla.wikimedia.org/show_bug.cgi?id=14202
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( "This is not a valid entry point to MediaWiki.\n" );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'Math',
+	'version' => '1.1',
+	'author' => array( 'Tomasz Wegrzanowski', 'Brion Vibber', '...' ),
+	'descriptionmsg' => 'math-desc',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:Math',
+);
+
+/**@{
+ * Maths constants
+ */
+define( 'MW_MATH_PNG',    0 );
+define( 'MW_MATH_SIMPLE', 1 ); /// @deprecated
+define( 'MW_MATH_HTML',   2 ); /// @deprecated
+define( 'MW_MATH_SOURCE', 3 );
+define( 'MW_MATH_MODERN', 4 ); /// @deprecated
+define( 'MW_MATH_MATHML', 5 ); /// @deprecated
+define( 'MW_MATH_MATHJAX', 6 ); /// new in 1.19/1.20
+define( 'MW_MATH_LATEXML', 7 ); /// new in 1.22
+/**@}*/
+
+/** For back-compat */
+$wgUseTeX = true;
+
+/** Location of the texvc binary */
+$wgTexvc = dirname( __FILE__ ) . '/math/texvc';
+/**
+ * Texvc background color
+ * use LaTeX color format as used in \special function
+ * for transparent background use value 'Transparent' for alpha transparency or
+ * 'transparent' for binary transparency.
+ */
+$wgTexvcBackgroundColor = 'transparent';
+
+/**
+ * Normally when generating math images, we double-check that the
+ * directories we want to write to exist, and that files that have
+ * been generated still exist when we need to bring them up again.
+ *
+ * This lets us give useful error messages in case of permission
+ * problems, and automatically rebuild images that have been lost.
+ *
+ * On a big site with heavy NFS traffic this can be slow and flaky,
+ * so sometimes we want to short-circuit it by setting this to false.
+ */
+$wgMathCheckFiles = true;
+
+/**
+ * The URL path of the math directory. Defaults to "{$wgUploadPath}/math".
+ *
+ * See http://www.mediawiki.org/wiki/Manual:Enable_TeX for details about how to
+ * set up mathematical formula display.
+ */
+$wgMathPath = false;
+
+/**
+ * The name of a file backend ($wgFileBackends) to use for storing math renderings.
+ * Defaults to FSFileBackend using $wgMathDirectory as a base path.
+ *
+ * See http://www.mediawiki.org/wiki/Manual:Enable_TeX for details about how to
+ * set up mathematical formula display.
+ */
+$wgMathFileBackend = false;
+
+/**
+ * The filesystem path of the math directory.
+ * Defaults to "{$wgUploadDirectory}/math".
+ *
+ * See http://www.mediawiki.org/wiki/Manual:Enable_TeX for details about how to
+ * set up mathematical formula display.
+ */
+$wgMathDirectory = false;
+
+/**
+ * Experimental option to use MathJax library to do client-side math rendering
+ * when JavaScript is available. In supporting browsers this makes nice output
+ * that's scalable for zooming, printing, and high-resolution displays.
+ *
+ * Not guaranteed to be stable at this time.
+ */
+$wgUseMathJax = false;
+
+/**
+ * Use of LaTeXML for details see
+ * <http://latexml.mathweb.org/help>
+ *
+ * If you want or need to run your own server, follow these installation
+ * instructions and override $wgLaTeXMLUrl:
+ * <https://svn.mathweb.org/repos/LaTeXML/branches/arXMLiv/INSTALL>
+ *
+ * If you expect heavy load you can specify multiple servers. In that case one
+ * server is randomly chosen for each rendering process. Specify the list of
+ * servers in an array e.g $wgLaTeXMLUrl = array ( 'http://latexml.example.com/convert',
+ * 'http://latexml2.example.com/convert');
+ */
+$wgLaTeXMLUrl = 'http://latexml.mathweb.org/convert';
+
+/**
+ * Allows to use LaTeXML as renderer for mathematical equation.
+ */
+$wgUseLaTeXML = false;
+
+/**
+ * The timeout for the HTTP-Request sent to the LaTeXML to render an equation,
+ * in seconds.
+ */
+$wgLaTeXMLTimeout = 240;
+
+////////// end of config settings.
+
+$wgDefaultUserOptions['math'] = MW_MATH_PNG;
+
+$wgExtensionFunctions[] = 'MathHooks::setup';
+$wgHooks['ParserFirstCallInit'][] = 'MathHooks::onParserFirstCallInit';
+$wgHooks['GetPreferences'][] = 'MathHooks::onGetPreferences';
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'MathHooks::onLoadExtensionSchemaUpdates';
+$wgHooks['ParserTestTables'][] = 'MathHooks::onParserTestTables';
+$wgHooks['ParserTestParser'][] = 'MathHooks::onParserTestParser';
+$wgHooks['UnitTestsList'][] = 'MathHooks::onRegisterUnitTests';
+
+$dir = dirname( __FILE__ ) . '/';
+$wgAutoloadClasses['MathHooks'] = $dir . 'Math.hooks.php';
+$wgAutoloadClasses['MathRenderer'] = $dir . 'MathRenderer.php';
+$wgAutoloadClasses['MathTexvc'] = $dir . 'MathTexvc.php';
+$wgAutoloadClasses['MathSource'] = $dir . 'MathSource.php';
+$wgAutoloadClasses['MathMathJax'] = $dir . 'MathMathJax.php';
+$wgAutoloadClasses['MathLaTeXML'] = $dir . 'MathLaTeXML.php';
+$wgExtensionMessagesFiles['Math'] = $dir . 'Math.i18n.php';
+
+$wgParserTestFiles[] = $dir . 'mathParserTests.txt';
+
+$moduleTemplate = array(
+	'localBasePath' => dirname( __FILE__ ) . '/modules',
+	'remoteExtPath' => 'Math/modules',
+);
+
+$wgResourceModules['ext.math.mathjax'] = array(
+	'scripts' => array(
+		'MathJax/MathJax.js',
+		// We'll let the other parts be loaded by MathJax's
+		// own module/config loader.
+	),
+	'group' => 'ext.math.mathjax',
+) + $moduleTemplate;
+
+$wgResourceModules['ext.math.mathjax.enabler'] = array(
+	'scripts' => 'ext.math.mathjax.enabler.js',
+) + $moduleTemplate;
