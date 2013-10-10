5a0ae2d453f3e7f8cb4edcfb30c914dbb4fd1299
David Mudrák <david@moodle.com>
2013-10-10 16:15:37 +0200

    Do not use hardcoded paths in our tool scripts
    This allows their execution at any host (read: my notebook as well as the
    production site).

--- tools/dump-all-docs.sh
+++ tools/dump-all-docs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-TARGET=/var/www/vhosts/docs.moodle.org/backups
 DBUSER=docs
 DBPASSWD=bhfd674b34hfd
+
+MYFULLPATH=$(readlink -f "$0")
+MYDIR=$(dirname "$MYFULLPATH")
+BASEDIR=$(dirname "$MYDIR")
+TARGET=$BASEDIR/backups/$(date +%Y-%m-%d-%H-%M)
 
 DBS=$(mysql -u $DBUSER -p$DBPASSWD --skip-column-names --execute="SELECT GROUP_CONCAT(schema_name SEPARATOR ' ') FROM information_schema.schemata WHERE schema_name LIKE '%docs_%'" --batch)
 
@@ -15,6 +19,7 @@ read -p "Press y to continue" -n 1 -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+	mkdir -p $TARGET
 	for DB in $DBS; do
 		echo "Dumping $DB"
 		mysqldump -u $DBUSER -p$DBPASSWD $DB > $TARGET/$DB.sql
