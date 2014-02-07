6dea29d56c5254f54b3a49ff669acdca22b2ed00
David Mudrák <david@moodle.com>
2014-02-07 11:53:53 +0100

    The moodle.org database has been moved to a new host

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -559,7 +559,7 @@ require_once( 'extensions/AuthMoodle.php' );
 $wgAuth = new AuthMoodle();
 $wgAuth->setAuthMoodleDBType('mysql');
 $wgAuth->setAuthMoodleTablePrefix('');
-$wgAuth->setAuthMoodleDBServer('db02.srv.in.moodle.com');
+$wgAuth->setAuthMoodleDBServer('education-secondary.srv.in.moodle.com');
 $wgAuth->setAuthMoodleDBName('moodleorg');
 $wgAuth->setAuthMoodleUser('moodleorg');
 $wgAuth->setAuthMoodlePassword('shai7eiC5Hajooje9nei');
